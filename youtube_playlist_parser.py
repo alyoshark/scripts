3a29b27fb2ac1ae9796a7ce0f8b4b632adf456c9
xch91 <chenhong.xie@gmail.com>
2013-07-06 22:49:24 +0800

    Update youtube_playlist_parser.py

--- youtube_playlist_parser.py
+++ youtube_playlist_parser.py
@@ -3,7 +3,12 @@ import sys
 
 with open(sys.argv[1]) as f:
     lines = f.read().split()
+    urls = []
     for l in lines:
         url = re.match(r'href=\"(/watch\?v=.*?)&amp;list=', l)
         if url:
-            print "http://youtube.com" + url.group(1)
+            urls.append("http://youtube.com" + url.group(1))
+
+    uniq_urls = set(urls)
+    for url in uniq_urls:
+        print url
