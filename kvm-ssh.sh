3c156a14c65a2da138d7d775de48af854d9196e6
Xie Chenhong <xch@linux-biuz.site>
2013-11-30 21:23:07 +0800

    A more usable kvm-ssh utility

--- kvm-ssh.sh
+++ kvm-ssh.sh
@@ -1,9 +1,17 @@
+#!/usr/bin/env bash
+
 # The virtual machine has to have Ports opened and sshd started
 # sudo systemctl start sshd.service
-# sudo echo "Port 21" > /etc/sshd_config
+# sudo echo "Port 22" > /etc/sshd_config
+# sudo echo "Port 9422" > /etc/sshd_config
 
 # Gets the IP of virtual machine
-addr=`arp -an | grep virbr | cut -d'(' -f2 | cut -d')' -f1`
+macaddr=`sudo virsh dumpxml "$1" | grep 'mac address' | cut -d\' -f2`
+ipaddr=`arp | grep "$macaddr" | cut -d' ' -f1`
+
+echo "$ipaddr"
 
 # And we can now happily ssh to it :)
-ssh -X "$addr"
+if [[ "$2" -eq "ssh" ]]; then
+    ssh -X "$ipaddr"
+fi
